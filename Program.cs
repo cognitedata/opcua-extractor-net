4e26f6c73bbd19af26a318f37766696729134030
Einar Marstrander Omang <einar.omang@cognite.com>
2019-06-25 11:29:50 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace opcua_extractor_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
