324bbcaeea0b4b58fdcc8f7f49c3a5b5bcd72c4c
Einar Marstrander Omang <einar.omang@cognite.com>
2019-07-19 11:13:17 +0200

    Add tests for the CDF pusher.
    This required creating a DummyFactory class which mocks the CDF interface.

---
+++ Testing/CDFPusherTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Cognite.OpcUa;
+using Xunit;
+
+namespace Testing
+{
+    public class CDFPusherTests
+    {
+        [Trait("Category", "fullserver")]
+        [Trait("Tests", "cdfpusher")]
+        [Fact]
+        public async Task TestBasicPushing()
+        {
+            FullConfig fullConfig = Utils.GetConfig("config.test.yml");
+            if (fullConfig == null) return;
+            Logger.Startup(fullConfig.LoggerConfig);
+            UAClient client = new UAClient(fullConfig);
+            var pusher = new CDFPusher(new DummyFactory(fullConfig.CogniteConfig.Project), fullConfig);
+
+            Extractor extractor = new Extractor(fullConfig, pusher, client);
+            extractor.Start();
+            if (!extractor.Started)
+            {
+                Logger.Shutdown();
+                return;
+            }
+            await extractor.MapUAToCDF();
+            Thread.Sleep(4000);
+            extractor.Close();
+        }
+    }
+}
--- Testing/UAClientTests.cs
+++ Testing/UAClientTests.cs
@@ -10,7 +10,7 @@ using Xunit;
 
 namespace Testing
 {
-    public class PusherTests
+    public class UAClientTests
     {
         [Trait("Category", "basicserver")]
         [Trait("Tests", "Mapping")]
