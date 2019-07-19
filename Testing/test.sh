194ee74abf7d2c90a16b13a661c875c2826d7e11
Einar Marstrander Omang <einar.omang@cognite.com>
2019-07-19 13:26:26 +0200

    Fixed issue where test server wouldn't properly restart

--- Testing/test.sh
+++ Testing/test.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 screen -dmS uaserver python3 servers/server-test1.py
-sleep 2
+sleep 3
 dotnet test --filter "Category=basicserver|Category=both" /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:CoverletOutput='coverage.json'
-screen -X -S uaserver stuff $'\003'
-sleep 1
+while screen -list | grep -q "uaserver"
+do
+    screen -X -S uaserver stuff $'\003'
+    sleep 1
+done
 screen -dmS uaserver python3 servers/server-test2.py
-sleep 2
+sleep 5
 dotnet test --filter "Category=fullserver|Category=both" /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:MergeWith='coverage.json' /p:CoverletOutputFormat=lcov /p:CoverletOutput='../coverage.lcov'
 screen -X -S uaserver stuff $'\003'
 rm coverage.json
