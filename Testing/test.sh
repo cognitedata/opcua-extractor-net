ea217fa307d2e0e844c7b635c0b45b3404562d18
Einar Marstrander Omang <einar.omang@cognite.com>
2019-07-19 14:25:36 +0200

    Made the test shellscript behave
    Bash can be a pain, and today is no exception. This should make it so that if the normal terminate command isn't good enough we fire a proper kill which should do it. Killing the screen didn't work at all, since the python process remained behind.

--- Testing/test.sh
+++ Testing/test.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
+
+killservers()
+{
+    screen -X -S uaserver stuff $'\003'
+    sleep 0.5
+    if screen -list | grep -q "uaserver"
+    then
+        kill $(ps aux | grep '[P]ython servers' | awk '{print $2}')
+    fi
+}
+killservers
 screen -dmS uaserver python3 servers/server-test1.py
 sleep 3
 dotnet test --filter "Category=basicserver|Category=both" /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:CoverletOutput='coverage.json'
-while screen -list | grep -q "uaserver"
-do
-    screen -X -S uaserver stuff $'\003'
-    sleep 1
-done
+killservers
 screen -dmS uaserver python3 servers/server-test2.py
 sleep 5
 dotnet test --filter "Category=fullserver|Category=both" /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:MergeWith='coverage.json' /p:CoverletOutputFormat=lcov /p:CoverletOutput='../coverage.lcov'
-screen -X -S uaserver stuff $'\003'
+killservers
 rm coverage.json
