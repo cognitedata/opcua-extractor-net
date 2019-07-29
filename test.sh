a3d16fe271cd13ae4694f8fd5a5dfaa42556536f
Einar Marstrander Omang <einar.omang@cognite.com>
2019-07-29 11:50:39 +0200

    Renamed projects to fit with .NET standards

--- test.sh
+++ test.sh
@@ -2,14 +2,14 @@
 
 killservers()
 {
-    kill $(ps aux | grep '[P]ython Testing/servers' | awk '{print $2}') > /dev/null 2>&1
+    kill $(ps aux | grep '[P]ython Test/servers' | awk '{print $2}') > /dev/null 2>&1
 }
 killservers
-python3 Testing/servers/server-test1.py > /dev/null 2>&1 &
+python3 Test/servers/server-test1.py > /dev/null 2>&1 &
 sleep 3
 dotnet test --filter "Category=basicserver|Category=both" /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:CoverletOutput='coverage.json'
 killservers
-python3 Testing/servers/server-test2.py > /dev/null 2>&1 &
+python3 Test/servers/server-test2.py > /dev/null 2>&1 &
 sleep 5
 dotnet test --filter "Category=fullserver|Category=both" /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:MergeWith='coverage.json' /p:CoverletOutputFormat=lcov /p:CoverletOutput='../coverage.lcov'
 killservers
