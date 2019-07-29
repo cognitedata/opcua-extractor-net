c2c57ae0cc912cdaeb6ac05d38f547bde5971640
Einar Marstrander Omang <einar.omang@cognite.com>
2019-07-29 11:09:20 +0200

    Made test.sh run from top level

--- test.sh
+++ test.sh
@@ -2,14 +2,14 @@
 
 killservers()
 {
-    kill $(ps aux | grep '[P]ython servers' | awk '{print $2}')
+    kill $(ps aux | grep '[P]ython Testing/servers' | awk '{print $2}') > /dev/null 2>&1
 }
 killservers
-python3 servers/server-test1.py &> /dev/null &
+python3 Testing/servers/server-test1.py > /dev/null 2>&1 &
 sleep 3
 dotnet test --filter "Category=basicserver|Category=both" /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:CoverletOutput='coverage.json'
 killservers
-python3 servers/server-test2.py &
+python3 Testing/servers/server-test2.py > /dev/null 2>&1 &
 sleep 5
 dotnet test --filter "Category=fullserver|Category=both" /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:MergeWith='coverage.json' /p:CoverletOutputFormat=lcov /p:CoverletOutput='../coverage.lcov'
 killservers
