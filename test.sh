414520e3a3e2028315eb04c2ddd2dc518e4a505b
Einar Marstrander Omang <einar.omang@cognite.com>
2019-07-31 08:47:02 +0200

    Fix coverage format

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dotnet test /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:CoverletOutput='../coverage.lcov'
+dotnet test /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput='../coverage.lcov'
