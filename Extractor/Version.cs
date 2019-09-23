071496b4991cf4021bb0b78e8caf9748e3debbd6
Einar Omang <einar@omang.com>
2019-09-23 16:14:08 +0200

    Log version information on startup

---
+++ Extractor/Version.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Cognite.OpcUa
+{
+    public class Version
+    {
+        private static string Read(string property)
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream($"Cognite.OpcUa.Properties.{property}"))
+            using (StreamReader reader = new StreamReader(stream))
+                return reader.ReadToEnd().Trim();
+        }
+
+        public static string Status()
+        {
+            var hash = Read("GitCommitHash");
+            var time = Read("GitCommitTime");
+            return $"{hash} {time}";
+        }
+        public static string GetVersion()
+        {
+            var raw = Read("GitCommitHash");
+            Regex rgx = new Regex(@"-(\d+)-*.*");
+            return rgx.Replace(raw, "-pre.$1", 1);
+        }
+    }
+}
