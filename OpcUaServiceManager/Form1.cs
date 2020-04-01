56c3d8e8ffbfbffe391164c680ee193bbcfc8bb0
John Berntsen <john.berntsen@cognite.com>
2020-04-01 13:26:07 +0200

    Added service manager

---
+++ OpcUaServiceManager/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceProcess;
+using Microsoft.Win32;
+using System.IO;
+
+namespace OpcUaServiceManager
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Private vars
+        /// </summary>
+        private const string _serviceBaseName = "opcuaext";
+        private ServiceController[] _winServices;
+        private List<ServiceController> _cogniteServices;
+        private string _opcuaExtractorDir;
+        private string _opcuaExtractorExe = @"OpcUaExtractor\OpcUaService.exe";
+        private bool _piextractorCanCreateServices = false;
+        private int _nextServiceNumber;
+
+        /// <summary>
+        /// Initialize the form, and validate OPCUA extractor install and that we have the service .exe file
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set some default values
+            lblOpcUaExtractorFound.Text = "OpcUaService executeable not found.";
+            lblOpcUaExtractorFound.ForeColor = Color.Red;
+            lblCmdRunStatus.Text = "";
+            lblServices.ForeColor = Color.Green;
+
+            GetOpcUaExtractorServices();
+
+            // Get installdir from registry, created by the installer.
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\Cognite\OpcUaExtractor");
+
+            if (key != null)
+            {
+                _opcuaExtractorDir = key.GetValue("InstallFolder").ToString();
+            }
+
+            if (File.Exists(_opcuaExtractorDir + _opcuaExtractorExe))
+            {
+                lblOpcUaExtractorFound.Text = "OpcUaService found: " + _opcuaExtractorDir + _opcuaExtractorExe;
+                lblOpcUaExtractorFound.ForeColor = Color.Green;
+                _piextractorCanCreateServices = true;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new PI extractor service
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSvcCreate_Click(object sender, EventArgs e)
+        {
+            lblCmdRunStatus.Text = "";
+
+            txtSvcDescription.Text = txtSvcDescription.Text.Replace(Environment.NewLine, " ");
+            txtSvcFolder.Text = txtSvcFolder.Text.Trim();
+            txtSvcName.Text = txtSvcName.Text.Trim();
+
+            if (!_piextractorCanCreateServices)
+            {
+                MessageBox.Show("OpcUa Extractor setup is wrong. Operation cancelled.");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtSvcName.Text))
+            {
+                MessageBox.Show("You need to provide a name for the service.");
+                return;
+            }
+
+            if (!Directory.Exists(txtSvcFolder.Text))
+            {
+                MessageBox.Show("Folder specified does not exist.");
+                return;
+            }
+
+            string result = RunCommand.Run(string.Format(@"/C sc create {2} binPath= ""\""{0}\"" \""{1}\"" \""{3}\"""" DisplayName= ""{3}""", _opcuaExtractorDir + _opcuaExtractorExe, txtSvcFolder.Text, _serviceBaseName + _nextServiceNumber, txtSvcName.Text));
+            if (result.Contains("SUCCESS"))
+            {
+                RunCommand.Run(string.Format(@"/C sc description {0} ""{1}""", _serviceBaseName + _nextServiceNumber, txtSvcDescription.Text));
+                result = result.Replace("[SC] ", "");
+                lblCmdRunStatus.Text = result;
+                lblCmdRunStatus.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblCmdRunStatus.Text = "Failed to create service.";
+                lblCmdRunStatus.ForeColor = Color.Red;
+            }
+
+            GetOpcUaExtractorServices();
+
+        }
+
+        /// <summary>
+        /// Opens a folder select dialog, for use as working dir for the extractor service
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSelectSvcFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtSvcFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// Deletes the selected windows service
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnDeleteService_Click(object sender, EventArgs e)
+        {
+            lblCmdRunStatus.Text = "";
+            ServiceController svcSelected = (ServiceController)listBoxOpcUaServices.SelectedItem;
+
+            if (svcSelected == null)
+            {
+                lblCmdRunStatus.Text = "No service to delete.";
+                lblCmdRunStatus.ForeColor = Color.Red;
+                return;
+            }
+
+            DialogResult userCheck = MessageBox.Show("Are you sure you want to delete this service ?", "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (userCheck == DialogResult.Yes)
+            {
+                string result = RunCommand.Run(string.Format(@"/C sc delete {0}", svcSelected.ServiceName));
+                if (result.Contains("SUCCESS"))
+                {
+                    result = result.Replace("[SC] ", "");
+                    lblCmdRunStatus.Text = result;
+                    lblCmdRunStatus.ForeColor = Color.Green;
+                }
+                else
+                {
+                    lblCmdRunStatus.Text = "Failed to delete service.";
+                    lblCmdRunStatus.ForeColor = Color.Red;
+                }
+
+                GetOpcUaExtractorServices();
+            }
+
+        }
+
+        /// <summary>
+        /// Finds and updates list of all pi extractor services this tool can control 
+        /// </summary>
+        private void GetOpcUaExtractorServices()
+        {
+            // Get all windows services, and create a list of our custom pi extractor services.
+            _winServices = ServiceController.GetServices();
+            _cogniteServices = new List<ServiceController>();
+            _nextServiceNumber = 1;
+
+            List<int> serviceNumbersUsed = new List<int>();
+
+            foreach (ServiceController sc in _winServices)
+            {
+                if (sc.ServiceName.StartsWith(_serviceBaseName))
+                {
+                    _cogniteServices.Add(sc);
+
+                    string num = sc.ServiceName.Replace(_serviceBaseName, "");
+                    if (!string.IsNullOrWhiteSpace(num))
+                    {
+                        serviceNumbersUsed.Add(Convert.ToInt32(num));
+                    }
+                }
+            }
+
+            if (serviceNumbersUsed != null && serviceNumbersUsed.Count > 0)
+            {
+                _nextServiceNumber = serviceNumbersUsed.Max() + 1;
+            }
+
+            // Show our services already made in the listbox
+            listBoxOpcUaServices.DataSource = _cogniteServices;
+            listBoxOpcUaServices.DisplayMember = "DisplayName";
+
+        }
+
+    }
+
+}
---
+++ OpcUaServiceManager/README.txt
@@ -0,0 +1,48 @@
+﻿Note, you will need permissions to edit windows services for this tool to work.
+
+This tool can create new OpcUa extractor services, to extract data from multiple OpcUa servers using one server.
+The installer creates a default service, which you will not see using this tool.
+
+It will check for a registry entry made by the installer and try and locate the PiExtractorService.exe
+If this file isnt found you wont be able to create new services.
+
+Explanation of the tool:
+
+Extractor Services: Lists services created by this tool, finds all services with name starting with "opcuaext"
+Delete Service:		Will delete the selected service after confirmation
+
+Create new service:
+Name:				Display name, name of service as displayed in 'services.msc' - Need to be unique.
+					This name will also be used as prefix in eventlog messages from the service.
+Description:		Add some description about the OpcUa service this extractor is connected too.
+Working Dir:		Need to be unique for each service you create, it will also require a config folder inside it
+					with a config.yml file unique to this extractor service.
+
+Create Service:		If information provided above is correct will create a new windows service.
+
+The newly created service will have no restart options and start type: Manual
+
+Now is the time to create a config\config.yml inside the 'Working Dir' specified above, start the service.
+You also need to copy over the 'Certificates' folder from the main extractor folder to 'Working Dir'
+After starting it: inspect the Application eventlog and also the log file inside the 'logs' folder created by the extractor.
+
+Once you have verified its working, set service startup to 'Automatic (Delayed Start)' and under the Recovery tab set all 3
+failure options to 'Restart the Service'
+
+Example folder structure for 3 services, which would each have their own 'extractorX' folder as working dir.
+If possible try and use another disk other than C: which is usually the OS disk.
+
+D:\OPCUAEXTRACTOR
+├───extractor1
+│   ├───certificates
+│   ├───config\config.yml
+│   └───logs
+├───extractor2
+│   ├───certificates
+│   ├───config\config.yml
+│   └───logs
+└───extractor3
+│   ├───certificates
+    ├───config\config.yml
+    └───logs
+
