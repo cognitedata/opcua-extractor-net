1fed36833a2930f4974d556b135eb313f4d59d15
Einar Omang <einar@omang.com>
2020-07-23 17:04:29 +0200

    2019->2020

--- Extractor/FailureBuffer.cs
+++ Extractor/FailureBuffer.cs
@@ -1,5 +1,5 @@
 ﻿/* Cognite Extractor for OPC-UA
-Copyright (C) 2019 Cognite AS
+Copyright (C) 2020 Cognite AS
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
