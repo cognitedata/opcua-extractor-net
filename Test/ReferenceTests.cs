fafabe4d7531a50612fd16dcdaa381bb1a4c93dc
Einar Omang <einar@omang.com>
2020-11-09 09:16:31 +0100

    Add mqtt pusher test

--- Test/ReferenceTests.cs
+++ Test/ReferenceTests.cs
@@ -14,31 +14,18 @@ namespace Test
     [Collection("Extractor tests")]
     public class ReferenceTests : MakeConsoleWork
     {
-        private readonly ILogger log = Log.Logger.ForContext(typeof(ReferenceTests));
+        // private readonly ILogger log = Log.Logger.ForContext(typeof(ReferenceTests));
 
         public ReferenceTests(ITestOutputHelper output) : base(output) { }
 
-
-        [Fact]
-        [Trait("Server", "array")]
-        [Trait("Target", "References")]
-        [Trait("Test", "basicreferences")]
-        public async Task TestBasicReferences()
+        private static async Task RunReferenceExtraction(ExtractorTester tester)
         {
-            using var tester = new ExtractorTester(new ExtractorTestParameters
-            {
-                ServerName = ServerName.Array,
-                QuitAfterMap = true,
-                References = true
-            });
             await tester.ClearPersistentData();
 
             tester.Config.Extraction.DataTypes.AllowStringVariables = true;
             tester.Config.Extraction.DataTypes.MaxArraySize = 4;
 
             await tester.StartServer();
-            tester.Server.PopulateArrayHistory();
-
             tester.Config.Extraction.DataTypes.CustomNumericTypes = new[]
             {
                 tester.IdToProtoDataType(tester.Server.Ids.Custom.MysteryType),
@@ -51,33 +38,6 @@ namespace Test
 
             tester.StartExtractor();
             await tester.TerminateRunTask(false);
-
-            var rels = tester.Handler.Relationships.Values;
-            foreach (var rel in rels)
-            {
-                log.Information("Relationship: {id}, {source}, {target}, {type1}, {type2}",
-                    rel.externalId, rel.sourceExternalId, rel.targetExternalId, rel.sourceType, rel.targetType);
-            }
-            Assert.Equal(8, rels.Count);
-            Assert.Equal(4, rels.Count(rel => rel.externalId.StartsWith("gp.HasSymmetricRelation", StringComparison.InvariantCulture)));
-            Assert.Equal(2, rels.Count(rel => rel.externalId.StartsWith("gp.HasCustomRelation", StringComparison.InvariantCulture)));
-            Assert.Equal(2, rels.Count(rel => rel.externalId.StartsWith("gp.IsCustomRelationOf", StringComparison.InvariantCulture)));
-
-            var assetRel = rels.First(rel => rel.externalId == "gp.IsCustomRelationOf;tl:i=1;tl:i=2");
-            Assert.Equal("Asset", assetRel.sourceType);
-            Assert.Equal("Asset", assetRel.targetType);
-            Assert.Equal("gp.tl:i=1", assetRel.sourceExternalId);
-            Assert.Equal("gp.tl:i=2", assetRel.targetExternalId);
-
-            var tsRel = rels.First(rel => rel.externalId == "gp.HasSymmetricRelation;tl:i=10;tl:i=8");
-            Assert.Equal("TimeSeries", tsRel.sourceType);
-            Assert.Equal("TimeSeries", tsRel.targetType);
-            Assert.Equal("gp.tl:i=10", tsRel.sourceExternalId);
-            Assert.Equal("gp.tl:i=8", tsRel.targetExternalId);
-
-            var arrayRel = rels.First(rel => rel.externalId == "gp.HasSymmetricRelation;tl:i=2;tl:i=3");
-            Assert.Equal("Asset", arrayRel.sourceType);
-            Assert.Equal("Asset", arrayRel.targetType);
         }
 
         [Fact]
@@ -169,12 +129,11 @@ namespace Test
             await tester.TerminateRunTask(false);
 
             var rels = tester.Handler.RelationshipsRaw.Values;
-            Assert.Equal(8, rels.Count);
-            foreach (var rel in rels)
-            {
-                log.Information("Relationship: {id}, {source}, {target}, {type1}, {type2}",
-                    rel.externalId, rel.sourceExternalId, rel.targetExternalId, rel.sourceType, rel.targetType);
-            }
+            TestRelationships(rels);
+        }
+        private static void TestRelationships(IEnumerable<RelationshipDummy> rels)
+        {
+            Assert.Equal(8, rels.Count());
             Assert.Equal(4, rels.Count(rel => rel.externalId.StartsWith("gp.HasSymmetricRelation", StringComparison.InvariantCulture)));
             Assert.Equal(2, rels.Count(rel => rel.externalId.StartsWith("gp.HasCustomRelation", StringComparison.InvariantCulture)));
             Assert.Equal(2, rels.Count(rel => rel.externalId.StartsWith("gp.IsCustomRelationOf", StringComparison.InvariantCulture)));
@@ -188,6 +147,43 @@ namespace Test
             Assert.Equal("TimeSeries", tsRel.sourceType);
             Assert.Equal("gp.tl:i=10", tsRel.sourceExternalId);
             Assert.Equal("gp.tl:i=8", tsRel.targetExternalId);
+        }
+
+        [Fact]
+        [Trait("Server", "array")]
+        [Trait("Target", "References")]
+        [Trait("Test", "basicreferences")]
+        public async Task TestBasicReferences()
+        {
+            using var tester = new ExtractorTester(new ExtractorTestParameters
+            {
+                ServerName = ServerName.Array,
+                QuitAfterMap = true,
+                References = true
+            });
+            await RunReferenceExtraction(tester);
+
+            var rels = tester.Handler.Relationships.Values;
+            TestRelationships(rels);
+        }
+
+        [Fact]
+        [Trait("Server", "array")]
+        [Trait("Target", "References")]
+        [Trait("Test", "mqttreferences")]
+        public async Task TestMqttReferences()
+        {
+            using var tester = new ExtractorTester(new ExtractorTestParameters
+            {
+                ServerName = ServerName.Array,
+                QuitAfterMap = true,
+                References = true,
+                Pusher = "mqtt"
+            });
+            await RunReferenceExtraction(tester);
+
+            var rels = tester.Handler.Relationships.Values;
+            TestRelationships(rels);
         }
     }
 }
