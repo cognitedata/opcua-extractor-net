9ade17fda8850a09853feb1c370f1b877e27eb93
Einar Omang <einar@omang.com>
2021-01-12 11:04:27 +0100

    Add test for creating ts update

--- Test/Unit/PusherUtilsTest.cs
+++ Test/Unit/PusherUtilsTest.cs
@@ -211,5 +211,95 @@ namespace Test.Unit
             Assert.Equal("value1", GetStringValue(meta, "prop1"));
             Assert.Equal("value4", GetStringValue(meta, "prop4"));
         }
+        [Fact]
+        public void TestGetTsUpdate()
+        {
+            using var extractor = tester.BuildExtractor();
+            var node = new UAVariable(new NodeId("test"), "test", new NodeId("parent"));
+            node.Description = "description";
+            node.Properties = new List<UAVariable>();
+            node.DataType = new UADataType(DataTypeIds.Boolean);
+            var now = DateTime.UtcNow;
+            for (int i = 1; i < 5; i++)
+            {
+                var prop = new UAVariable(new NodeId($"prop{i}"), $"prop{i}", NodeId.Null);
+                prop.SetDataPoint($"value{i}", now, tester.Client);
+                node.Properties.Add(prop);
+            }
+            // Need to create this manually to match
+            var ts = new TimeSeries
+            {
+                ExternalId = "gp.base:s=test",
+                Name = "test",
+                AssetId = 111,
+                Description = "description",
+                Metadata = new Dictionary<string, string>
+                {
+                    { "prop1", "value1" },
+                    { "prop2", "value2" },
+                    { "prop3", "value3" },
+                    { "prop4", "value4" }
+                }
+            };
+            var nodeToAssetIds = new Dictionary<NodeId, long>
+            {
+                { new NodeId("parent"), 111 },
+                { new NodeId("parent2"), 222 }
+            };
+
+            var update = new TypeUpdateConfig();
+
+            Assert.Null(PusherUtils.GetTSUpdate(extractor, null, node, update, nodeToAssetIds));
+            var result = PusherUtils.GetTSUpdate(extractor, ts, node, update, nodeToAssetIds);
+            Assert.Null(result.AssetId);
+            Assert.Null(result.Description);
+            Assert.Null(result.Metadata);
+            Assert.Null(result.Name);
+
+            update.Context = true;
+            update.Description = true;
+            update.Metadata = true;
+            update.Name = true;
+            result = PusherUtils.GetTSUpdate(extractor, ts, node, update, nodeToAssetIds);
+            Assert.Null(result.AssetId);
+            Assert.Null(result.Description);
+            Assert.Null(result.Metadata);
+            Assert.Null(result.Name);
+
+            // Update everything
+            var oldProperties = node.Properties;
+            node = new UAVariable(new NodeId("test2"), "test2", new NodeId("parent2"));
+            node.Description = "description2";
+            node.Properties = oldProperties;
+            oldProperties.RemoveAt(1);
+            oldProperties.Add(new UAVariable(new NodeId("prop-new"), "prop-new", NodeId.Null));
+            oldProperties[3].SetDataPoint("value-new", now, tester.Client);
+            oldProperties[2].SetDataPoint("value4-new", now, tester.Client);
+
+            result = PusherUtils.GetTSUpdate(extractor, ts, node, new TypeUpdateConfig(), nodeToAssetIds);
+            Assert.Null(result.AssetId);
+            Assert.Null(result.Description);
+            Assert.Null(result.Metadata);
+            Assert.Null(result.Name);
+
+            result = PusherUtils.GetTSUpdate(extractor, ts, node, update, nodeToAssetIds);
+            Assert.Equal("description2", result.Description.Set);
+            Assert.Equal("test2", result.Name.Set);
+            Assert.Equal(222, result.AssetId.Set);
+            Assert.Equal(4, result.Metadata.Add.Count);
+            Assert.Equal("value1", result.Metadata.Add["prop1"]);
+            Assert.False(result.Metadata.Add.ContainsKey("prop2"));
+            Assert.Equal("value3", result.Metadata.Add["prop3"]);
+            Assert.Equal("value4-new", result.Metadata.Add["prop4"]);
+            Assert.Equal("value-new", result.Metadata.Add["prop-new"]);
+
+            // Update with null values, and missing asset
+            node = new UAVariable(new NodeId("test3"), null, new NodeId("parent3"));
+            result = PusherUtils.GetTSUpdate(extractor, ts, node, update, nodeToAssetIds);
+            Assert.Null(result.AssetId);
+            Assert.Null(result.Description);
+            Assert.Null(result.Metadata);
+            Assert.Null(result.Name);
+        }
     }
 }
