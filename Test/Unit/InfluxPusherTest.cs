8c5ebecd35633312e979777ec7467ba1b8ced0f6
Einar Omang <einar@omang.com>
2021-01-25 15:47:02 +0100

    Remove InfluxDb.Client package
    Seems like this will be much more complicated than anticipated, and not
    really have the benefits hoped for.

--- Test/Unit/InfluxPusherTest.cs
+++ Test/Unit/InfluxPusherTest.cs
@@ -1,9 +1,5 @@
 ﻿using AdysTech.InfluxDB.Client.Net;
 using Cognite.OpcUa;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
