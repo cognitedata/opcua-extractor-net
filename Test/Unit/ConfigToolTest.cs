80a71ea85d7b7813df4c49036369918612a97a95
Einar Omang <einar@omang.com>
2021-02-15 09:19:35 +0100

    Add unit test file

---
+++ Test/Unit/ConfigToolTest.cs
@@ -0,0 +1,33 @@
+﻿using Opc.Ua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Utils;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Test.Unit
+{
+    public class ConfigToolTestFixture : BaseExtractorTestFixture
+    {
+        public ConfigToolTestFixture() : base(63400)
+        {
+            Config.Source.PublishingInterval = 200;
+            Config.Extraction.DataPushDelay = 200;
+            Config.History.Enabled = false;
+            Config.Events.Enabled = true;
+            Config.Extraction.RootNode = Server.Ids.Event.Root.ToProtoNodeId(Client);
+        }
+    }
+    public class ConfigToolTest : MakeConsoleWork, IClassFixture<ConfigToolTestFixture>
+    {
+        private readonly ConfigToolTestFixture tester;
+        public ConfigToolTest(ITestOutputHelper output, ConfigToolTestFixture tester) : base(output)
+        {
+            this.tester = tester;
+        }
+
+    }
+}
