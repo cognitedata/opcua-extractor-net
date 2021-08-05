22bc6411fb7498792310e8d04a9a70a78f3edd22
Einar Marstrander Omang <einar@omang.com>
2021-08-05 15:46:41 +0200

    Proof of concept multiplatform service
    Working on windows, probably working on linux

---
+++ Service/Program.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Cognite.Extractor.Logging;
+using Cognite.Extractor.Metrics;
+using Cognite.Extractor.StateStorage;
+using Cognite.Extractor.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Cognite.OpcUa.Service
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var rt = typeof(System.OperatingSystem);
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    string path;
+                    if (args.Length > 0)
+                    {
+                        path = args[0];
+                        if (!Directory.Exists(path))
+                        {
+                            throw new ConfigurationException($"Target directory does not exist: {path}");
+                        }
+                    }
+                    else
+                    {
+                        path = Directory.GetParent(AppContext.BaseDirectory).Parent.FullName;
+                    }
+                    Directory.SetCurrentDirectory(path);
+                    var configFile = "config/config.yml";
+                    var config = services.AddConfig<FullConfig>(configFile, 1);
+                    config.Source.ConfigRoot = "config/";
+                    services.AddMetrics();
+                    services.AddLogger();
+                    if (config.Cognite != null)
+                    {
+                        services.AddCogniteClient("OPC-UA Extractor", $"CogniteOPCUAExtractor/{Version.GetVersion()}", true, true, true);
+                    }
+                    services.AddStateStore();
+                    services.AddHostedService<Worker>();
+                })
+                .ConfigureLogging(loggerFactory => loggerFactory.AddEventLog())
+                .UseWindowsService()
+                .UseSystemd();
+    }
+}
---
+++ Service/Worker.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Cognite.OpcUa;
+using Serilog;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Prometheus;
+using Microsoft.Extensions.Logging;
+
+namespace Cognite.OpcUa.Service
+{
+    public class Worker : BackgroundService
+    {
+        private static readonly Gauge version =
+            Metrics.CreateGauge("opcua_version", $"version: {Version.GetVersion()}, status: {Version.Status()}");
+        private FullConfig config;
+        private readonly IServiceProvider provider;
+        private Microsoft.Extensions.Logging.ILogger eventLog;
+        public Worker(ILogger<Worker> eventLog, IServiceProvider provider)
+        {
+            this.provider = provider;
+            this.eventLog = eventLog;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Log.Logger = provider.GetRequiredService<Serilog.ILogger>();
+            config = provider.GetRequiredService<FullConfig>();
+            eventLog.LogInformation("Starting OPC UA Extractor service version {version}", Version.GetVersion());
+            eventLog.LogInformation("Revision information: {status}", Version.Status());
+
+            var runTime = new ExtractorRuntime(config, provider);
+
+            int waitRepeats = 0;
+
+            version.Set(0);
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+                DateTime startTime = DateTime.UtcNow;
+                try
+                {
+                    await runTime.Run(stoppingToken);
+                }
+                catch (TaskCanceledException)
+                {
+                }
+                catch (AggregateException aex)
+                {
+                    if (ExtractorUtils.GetRootExceptionOfType<ConfigurationException>(aex) != null)
+                    {
+                        eventLog.LogError("Invalid configuration, stopping: {msg}", aex.InnerException.Message);
+                        break;
+                    }
+                    if (ExtractorUtils.GetRootExceptionOfType<TaskCanceledException>(aex) != null)
+                    {
+                        eventLog.LogError("Extractor halted due to cancelled task");
+                    }
+                    else if (ExtractorUtils.GetRootExceptionOfType<SilentServiceException>(aex) == null)
+                    {
+                        eventLog.LogError(aex, "Unexpected failure in extractor: {msg}", aex.Message);
+                    }
+                }
+                catch (ConfigurationException)
+                {
+                    eventLog.LogError("Invalid configuration, stopping");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    eventLog.LogError(ex, "Unexpected failure in extractor: {msg}", ex.Message);
+                }
+
+                if (startTime > DateTime.UtcNow - TimeSpan.FromSeconds(600))
+                {
+                    waitRepeats++;
+                }
+                else
+                {
+                    waitRepeats = 0;
+                }
+
+                try
+                {
+                    var sleepTime = TimeSpan.FromSeconds(Math.Pow(2, Math.Min(waitRepeats, 9)));
+                    eventLog.LogInformation("Sleeping for {time}", sleepTime);
+                    await Task.Delay(sleepTime, stoppingToken);
+                }
+                catch (Exception)
+                {
+                    eventLog.LogWarning("Extractor stopped manually");
+                    break;
+                }
+
+                if (waitRepeats > 5) break;
+            }
+        }
+    }
+}
