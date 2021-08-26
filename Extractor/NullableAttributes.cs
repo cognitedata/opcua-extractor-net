cf881c691955ebdf32b56c1bebb4464e79505664
Einar Omang <einar@omang.com>
2021-08-26 10:00:55 +0200

    Hopefully fix coverage

---
+++ Extractor/NullableAttributes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    using global::System;
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    [ExcludeFromCodeCoverage, DebuggerNonUserCode]
+    internal sealed class MemberNotNullWhenAttribute : Attribute
+    {
+        public bool ReturnValue { get; }
+        public string[] Members { get; }
+        public MemberNotNullWhenAttribute(bool returnValue, string member)
+        {
+            ReturnValue = returnValue;
+            Members = new[] { member };
+        }
+        public MemberNotNullWhenAttribute(bool returnValue, params string[] members)
+        {
+            ReturnValue = returnValue;
+            Members = members;
+        }
+    }
+}
