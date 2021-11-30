fa1084f144d4ad724a6fbc51c5bb14ae24967867
Einar Omang <einar@omang.com>
2021-11-30 08:13:21 +0100

    Move update methods to controller

--- Server/Program.cs
+++ Server/Program.cs
@@ -26,22 +26,9 @@ namespace Server
                 //server.TriggerEvents(0);
                 //server.DirectGrowth(idx++);
                 //server.ReferenceGrowth(idx++);
-                server.UpdateNode(server.Ids.Base.DoubleVar1, idx);
-                server.UpdateNode(server.Ids.Base.DoubleVar2, -idx);
-                server.UpdateNode(server.Ids.Base.BoolVar, idx % 2 == 0);
-                server.UpdateNode(server.Ids.Base.IntVar, idx);
-                server.UpdateNode(server.Ids.Base.StringVar, $"Idx: {idx}");
 
-                server.UpdateNode(server.Ids.Custom.Array, new double[] { idx, idx + 1, idx + 2, idx + 3 });
-                server.UpdateNode(server.Ids.Custom.StringArray, new string[] { $"str{idx}", $"str{-idx}" });
-                server.UpdateNode(server.Ids.Custom.StringyVar, $"Idx: {idx}");
-                server.UpdateNode(server.Ids.Custom.MysteryVar, idx);
-                server.UpdateNode(server.Ids.Custom.IgnoreVar, $"Idx: {idx}");
-                server.UpdateNode(server.Ids.Custom.NumberVar, idx);
-                server.UpdateNode(server.Ids.Custom.EnumVar1, idx % 3);
-                server.UpdateNode(server.Ids.Custom.EnumVar2, idx % 2 == 0 ? 123 : 321);
-                server.UpdateNode(server.Ids.Custom.EnumVar3, idx % 2 == 0
-                    ? new[] { 123, 123, 321, 123 } : new[] { 123, 123, 123, 321 });
+                server.UpdateBaseNodes(idx);
+                server.UpdateCustomNodes(idx);
 
                 idx++;
                 Task.Delay(1000).Wait();
--- Server/ServerController.cs
+++ Server/ServerController.cs
@@ -282,5 +282,28 @@ namespace Server
         {
             Server.SetDiagnosticsEnabled(value);
         }
+
+        public void UpdateBaseNodes(int idx)
+        {
+            UpdateNode(Ids.Base.DoubleVar1, idx);
+            UpdateNode(Ids.Base.DoubleVar2, -idx);
+            UpdateNode(Ids.Base.BoolVar, idx % 2 == 0);
+            UpdateNode(Ids.Base.IntVar, idx);
+            UpdateNode(Ids.Base.StringVar, $"Idx: {idx}");
+        }
+
+        public void UpdateCustomNodes(int idx)
+        {
+            UpdateNode(Ids.Custom.Array, new double[] { idx, idx + 1, idx + 2, idx + 3 });
+            UpdateNode(Ids.Custom.StringArray, new string[] { $"str{idx}", $"str{-idx}" });
+            UpdateNode(Ids.Custom.StringyVar, $"Idx: {idx}");
+            UpdateNode(Ids.Custom.MysteryVar, idx);
+            UpdateNode(Ids.Custom.IgnoreVar, $"Idx: {idx}");
+            UpdateNode(Ids.Custom.NumberVar, idx);
+            UpdateNode(Ids.Custom.EnumVar1, idx % 3);
+            UpdateNode(Ids.Custom.EnumVar2, idx % 2 == 0 ? 123 : 321);
+            UpdateNode(Ids.Custom.EnumVar3, idx % 2 == 0
+                ? new[] { 123, 123, 321, 123 } : new[] { 123, 123, 123, 321 });
+        }
     }
 }
