774645f45ef0d21fbc7bdc435fabaa1136126825
Einar Marstrander Omang <einar@omang.com>
2022-03-25 08:11:10 +0100

    Reduce test verbosity a little
    New test logging system plays better with xunit, so we no longer need
    the -v, which now just creates an enormous amount of logs in jenkins.

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dotnet test -v n /p:Exclude="[xunit*]*%2c[Server]*%2c[System*]*" /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput='../coverage.lcov'
+dotnet test /p:Exclude="[xunit*]*%2c[Server]*%2c[System*]*" /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput='../coverage.lcov'
