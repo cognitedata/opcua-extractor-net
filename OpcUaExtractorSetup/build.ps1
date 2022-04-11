727a7401e0c2082f422133f94528ee5e532826f2
Einar Marstrander Omang <einar@omang.com>
2022-04-11 08:53:12 +0200

    Update installer build

---
+++ OpcUaExtractorSetup/build.ps1
@@ -0,0 +1,32 @@
+Param(
+	[Parameter(Mandatory=$true)]
+	[Alias("b")]
+	[string]$msbuild,
+	
+	[Parameter(Mandatory=$true)]
+	[Alias("v")]
+	[string]$version,
+	
+	[Parameter(Mandatory=$true)]
+	[Alias("d")]
+	[string]$description,
+	
+	[Parameter(Mandatory=$true)]
+	[Alias("c")]
+	[string]$config
+)
+
+$Args = ""
+$Settings = Get-Content -Path $config | ConvertFrom-Json
+foreach ($prop in $Settings.PsObject.Properties) {
+	if ($prop.Name -eq "setup_project") {
+		continue
+	}
+	$Args = "$($Args) /p:$($prop.Name)=`"$($prop.Value)`""
+}
+
+$Args = "$($Args) /p:target_version=`"$($version)`" /p:target_description=`"$($description)`" /p:Configuration=Release"
+
+$build_command = ".`"$msbuild`" $Args $($Settings.setup_project)"
+echo $build_command
+Invoke-Expression $build_command
