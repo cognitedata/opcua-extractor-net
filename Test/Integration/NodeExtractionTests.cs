33e85d1d5b299748d388b0652b23b52b2eeae9f5
Einar Marstrander Omang <einar@omang.com>
2022-08-19 12:56:38 +0200

    Fix one more test

--- Test/Integration/NodeExtractionTests.cs
+++ Test/Integration/NodeExtractionTests.cs
@@ -1138,7 +1138,7 @@ namespace Test.Integration
             await extractor.RunExtractor(true);
 
             Assert.Equal(475, pusher.PushedNodes.Count);
-            Assert.Equal(426, pusher.PushedVariables.Count);
+            Assert.Equal(418, pusher.PushedVariables.Count);
             var customVarType = pusher.PushedNodes[tester.Server.Ids.Custom.VariableType];
             Assert.Equal("CustomVariableType", customVarType.DisplayName);
             Assert.Equal(NodeClass.VariableType, customVarType.NodeClass);
