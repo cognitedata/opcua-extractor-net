d74a71e33d1059deabeb3cddabc8b13a33622a71
Einar Marstrander Omang <einar@omang.com>
2023-03-21 10:48:37 +0100

    Remove installerbuild folder

--- installer-sbom.ps1
+++ installer-sbom.ps1
@@ -4,21 +4,21 @@ Param(
 	[string]$version
 )
 
-dotnet sbom-tool generate -b installerbuild\extractorbuild\ -bc .\ExtractorLauncher\ -pn OpcuaExtractor -pv $version -ps Cognite -nsb https://sbom.cognite.com
-dotnet sbom-tool generate -b installerbuild\managerbuild\ -bc .\OpcUaServiceManager\ -pn OpcUaServiceManager -pv $version -ps Cognite -nsb https://sbom.cognite.com
+dotnet sbom-tool generate -b extractorbuild\ -bc .\ExtractorLauncher\ -pn OpcuaExtractor -pv $version -ps Cognite -nsb https://sbom.cognite.com
+dotnet sbom-tool generate -b managerbuild\ -bc .\OpcUaServiceManager\ -pn OpcUaServiceManager -pv $version -ps Cognite -nsb https://sbom.cognite.com
 
-mkdir installerbuild\manifests
+mkdir manifests
 
-mv installerbuild\extractorbuild\_manifest\spdx_2.2\manifest.spdx.json installerbuild\manifests\manifest.extractor.spdx.json
-mv installerbuild\extractorbuild\_manifest\spdx_2.2\manifest.spdx.json.sha256 installerbuild\manifests\manifest.extractor.spdx.json.sha256
-mv installerbuild\managerbuild\_manifest\spdx_2.2\manifest.spdx.json installerbuild\manifests\manifest.manager.spdx.json
-mv installerbuild\managerbuild\_manifest\spdx_2.2\manifest.spdx.json.sha256 installerbuild\manifests\manifest.manager.spdx.json.sha256
+mv extractorbuild\_manifest\spdx_2.2\manifest.spdx.json manifests\manifest.extractor.spdx.json
+mv extractorbuild\_manifest\spdx_2.2\manifest.spdx.json.sha256 manifests\manifest.extractor.spdx.json.sha256
+mv managerbuild\_manifest\spdx_2.2\manifest.spdx.json manifests\manifest.manager.spdx.json
+mv managerbuild\_manifest\spdx_2.2\manifest.spdx.json.sha256 manifests\manifest.manager.spdx.json.sha256
 
-rm -r installerbuild\extractorbuild\_manifest\
-rm -r installerbuild\managerbuild\_manifest\
+rm -r extractorbuild\_manifest\
+rm -r managerbuild\_manifest\
 
-dotnet sbom-tool generate -b installerbuild\manifests\ -pn OpcuaExtractor -pv $version -ps Cognite -nsb https://sbom.cognite.com
-mv installerbuild\manifests\_manifest\spdx_2.2\manifest.spdx.json installerbuild\manifests\manifest.spdx.json
-mv installerbuild\manifests\_manifest\spdx_2.2\manifest.spdx.json.sha256 installerbuild\manifests\manifest.spdx.json.sha256
+dotnet sbom-tool generate -b manifests\ -pn OpcuaExtractor -pv $version -ps Cognite -nsb https://sbom.cognite.com
+mv manifests\_manifest\spdx_2.2\manifest.spdx.json manifests\manifest.spdx.json
+mv manifests\_manifest\spdx_2.2\manifest.spdx.json.sha256 manifests\manifest.spdx.json.sha256
 
-rm -r installerbuild\manifests\_manifest\
+rm -r manifests\_manifest\
