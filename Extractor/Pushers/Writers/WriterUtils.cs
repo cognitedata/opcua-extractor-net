10465b6612e5b64f05af919233d155280160aec1
Einar <einar@omang.com>
2023-10-26 12:54:31 +0200

    Refactor CDF writers to include deletes (#514)
    This was meant to just be a PR to add deletes to FDM, but it became clear that the structure of the new writer setup was poorly suited for this, and there are also a few bugs in there. This refactors it (removing quite a few lines of code), and also moves deletes into the writers, which generally makes sense. The old approach didn't really make CDFPusher any more readable, which was the entire point.
    
    Actually implementing FDM deletes will come in a separate PR.

--- Extractor/Pushers/Writers/WriterUtils.cs
+++ Extractor/Pushers/Writers/WriterUtils.cs
@@ -1,9 +1,11 @@
-using System;
+using System.Collections.Generic;
+using System.Text.Json;
 using System.Threading;
+using System.Threading.Tasks;
 using Cognite.Extractor.Utils;
 using Cognite.OpcUa.Config;
 using Cognite.OpcUa.Pushers.FDM;
-using Cognite.OpcUa.Pushers.Writers.Interfaces;
+using CogniteSdk;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -13,30 +15,21 @@ namespace Cognite.OpcUa.Pushers.Writers
     {
         public static void AddWriters(this IServiceCollection services, FullConfig config)
         {
-            services.AddSingleton<ITimeseriesWriter>(provider => {
+            services.AddSingleton<BaseTimeseriesWriter>(provider =>
+            {
                 var destination = provider.GetRequiredService<CogniteDestination>();
                 var config = provider.GetRequiredService<FullConfig>();
-                return (config.Cognite?.MetadataTargets?.Clean?.Timeseries ?? false) 
+                return (config.Cognite?.MetadataTargets?.Clean?.Timeseries ?? false)
                     ? new TimeseriesWriter(provider.GetRequiredService<ILogger<TimeseriesWriter>>(), destination, config)
                     : new MinimalTimeseriesWriter(provider.GetRequiredService<ILogger<MinimalTimeseriesWriter>>(), destination, config);
             });
-            if (config.Cognite?.MetadataTargets?.Clean?.Assets ?? false)
+            if (config.Cognite?.MetadataTargets?.Clean is not null)
             {
-                services.AddSingleton<IAssetsWriter, AssetsWriter>(provider => {
+                services.AddSingleton(provider =>
+                {
                     var destination = provider.GetRequiredService<CogniteDestination>();
-                    return new AssetsWriter(
-                        provider.GetRequiredService<ILogger<AssetsWriter>>(),
-                        destination,
-                        config
-                    );
-                });
-            }
-            if (config.Cognite?.MetadataTargets?.Clean?.Relationships ?? false)
-            {
-                services.AddSingleton<IRelationshipsWriter, RelationshipsWriter>(provider => {
-                    var destination = provider.GetRequiredService<CogniteDestination>();
-                    return new RelationshipsWriter(
-                        provider.GetRequiredService<ILogger<RelationshipsWriter>>(),
+                    return new CleanWriter(
+                        provider.GetRequiredService<ILogger<CleanWriter>>(),
                         destination,
                         config
                     );
@@ -44,7 +37,8 @@ namespace Cognite.OpcUa.Pushers.Writers
             }
             if (config.Cognite?.MetadataTargets?.Raw is not null)
             {
-                services.AddSingleton<IRawWriter, RawWriter>(provider => {
+                services.AddSingleton(provider =>
+                {
                     var destination = provider.GetRequiredService<CogniteDestination>();
                     return new RawWriter(
                         provider.GetRequiredService<ILogger<RawWriter>>(),
@@ -55,22 +49,65 @@ namespace Cognite.OpcUa.Pushers.Writers
             }
             if (config.Cognite?.MetadataTargets?.DataModels != null && config.Cognite.MetadataTargets.DataModels.Enabled)
             {
-                services.AddSingleton(provider => {
+                services.AddSingleton(provider =>
+                {
                     var destination = provider.GetRequiredService<CogniteDestination>();
                     return new FDMWriter(provider.GetRequiredService<FullConfig>(), destination,
                         provider.GetRequiredService<ILogger<FDMWriter>>());
                 });
             }
-            services.AddSingleton<ICDFWriter, CDFWriter>(provider =>
+            services.AddSingleton(provider =>
             {
                 return new CDFWriter(
-                    provider.GetRequiredService<ITimeseriesWriter>(),
-                    provider.GetService<IRawWriter>(),
-                    provider.GetService<IAssetsWriter>(),
-                    provider.GetService<IRelationshipsWriter>(),
-                    provider.GetService<FDMWriter>()
+                    provider.GetRequiredService<BaseTimeseriesWriter>(),
+                    provider.GetService<RawWriter>(),
+                    provider.GetService<CleanWriter>(),
+                    provider.GetService<FDMWriter>(),
+                    provider.GetRequiredService<FullConfig>(),
+                    provider.GetRequiredService<ILogger<CDFWriter>>()
                 );
             });
+        }
+
+        public static async Task<IEnumerable<RawRow<Dictionary<string, JsonElement>>>> GetRawRows(
+            string dbName,
+            string tableName,
+            CogniteDestination destination,
+            IEnumerable<string>? columns,
+            ILogger log,
+            CancellationToken token
+        )
+        {
+            string? cursor = null;
+            var rows = new List<RawRow<Dictionary<string, JsonElement>>>();
+            do
+            {
+                try
+                {
+                    var result = await destination.CogniteClient.Raw.ListRowsAsync<
+                        Dictionary<string, JsonElement>
+                    >(
+                        dbName,
+                        tableName,
+                        new RawRowQuery
+                        {
+                            Cursor = cursor,
+                            Limit = 10_000,
+                            Columns = columns
+                        },
+                        null,
+                        token
+                    );
+                    rows.AddRange(result.Items);
+                    cursor = result.NextCursor;
+                }
+                catch (ResponseException ex) when (ex.Code == 404)
+                {
+                    log.LogWarning("Table or database not found: {Message}", ex.Message);
+                    break;
+                }
+            } while (cursor != null);
+            return rows;
         }
     }
 }
