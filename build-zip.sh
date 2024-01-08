8bd2d5106279ad5f276a1de115e2e6a7093dbd28
Ozan Goktan <ozan.goktan@cognite.com>
2024-01-08 14:31:12 +0100

    .net 8.0 everywhere

--- build-zip.sh
+++ build-zip.sh
@@ -6,7 +6,7 @@ linux="$2"
 version="$3"
 
 mkdir -p $configuration/
-mv ExtractorLauncher/bin/Release/net7.0/$configuration/publish/* ./$configuration/
+mv ExtractorLauncher/bin/Release/net8.0/$configuration/publish/* ./$configuration/
 rm -f ./$configuration/*.config ./$configuration/*.pdb ./$configuration/*.xml
 mkdir -p ./$configuration/config
 
