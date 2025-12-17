3f11cec057db4eb63a2f85ccdf297e7fc08f9624
Ozan Goktan <ozan.goktan@cognite.com>
2025-12-17 13:24:31 +0100

    remove method that has migrated to utils

--- Extractor/Pushers/CDFPusher.cs
+++ Extractor/Pushers/CDFPusher.cs
@@ -16,6 +16,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
 
 using Cognite.Extensions;
+using Cognite.Extractor.Common;
 using Cognite.Extractor.Utils;
 using Cognite.OpcUa.Config;
 using Cognite.OpcUa.History;
